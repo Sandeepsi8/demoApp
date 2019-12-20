b88905f30c7868671c29e571441515c8620d58d6
Sandeep Isher <sandeep.si817@gmail.com>
2019-12-19 16:37:28 -0800

    Initial Commit

---
+++ Tippy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tippy
+//
+//  Created by Sandeep  on 12/19/19.
+//  Copyright © 2019 Sandeep . All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
